4ecb563d44cbd1ce2b643dba57cfd855dc96ca14
Muhammad ahmed noorani noorani <159555834+Ahmednoorani258@users.noreply.github.com>
2024-10-05 11:58:23 +0000

    update

--- music_academy/src/app/page.tsx
+++ music_academy/src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 import FeaturedCourses from "@/components/FeaturedCourses";
 import TestimonialCard from "@/components/TestimonialCards";
+import UpcomingWebinars from "@/components/UpcomingWEbinars";
+import Instructor from "@/components/Instructor";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <HeroSection/>
       <FeaturedCourses/>
       <TestimonialCard/>
+      <UpcomingWebinars/>
+      <Instructor/>
     </main>
   );
 }
